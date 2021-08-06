6a50416acf9d550a0853334d76d0ef711afd56ab
Joao Viana <joaopadraoviana@gmail.com>
2021-08-06 21:19:09 +0100

    refactor: improve util

--- src/index.ts
+++ src/index.ts
@@ -9,27 +9,24 @@ type CSSMediaQueries = 'max-width' | 'min-width' | 'max-height' | 'min-height';
 type Breakpoints = (
   rule: keyof CSSProperties,
   unit: CSSPropUnits,
-  values: Record<number, number | string>[],
+  values: Record<number, number | string>,
   mediaQueryType?: CSSMediaQueries,
   important?: boolean
 ) => FlattenSimpleInterpolation;
 
-// TODO: documentation
 export const breakpoints: Breakpoints = (
   rule = 'padding',
   unit = 'px',
-  values: Record<number, number | string>[],
+  values,
   mediaQueryType = 'max-width'
 ) =>
-  // FIXME: find a way to fix CSSObject error
-  // @ts-expect-error
-  css([
-    values.reduce((mediaQueries, value) => {
-      const [breakpointValue, ruleValue] = Object.entries(value);
-      return (mediaQueries += `
-    @media screen and (${mediaQueryType}: ${breakpointValue}px) {
-      ${rule}: ${ruleValue}${unit};
-    }
-    `);
-    }, ''),
-  ]);
+  css(([
+    Object.entries(values).reduce(
+      (mediaQueries, [breakpointValue, ruleValue]) =>
+        (mediaQueries += `
+        @media screen and (${mediaQueryType}: ${breakpointValue}px) {
+          ${rule}: ${ruleValue}${unit};
+        }`),
+      ''
+    ),
+  ] as unknown) as TemplateStringsArray);
--- test/breakpoints.test.ts
+++ test/breakpoints.test.ts
@@ -3,40 +3,30 @@ import { breakpoints } from '../src';
 describe('breakpoints', () => {
   it('works', () => {
     expect(
-      breakpoints('width', 'vw', [
-        { 1600: 40 },
-        { 1400: 40 },
-        { 1400: 40 },
-        { 1280: 60 },
-        { 992: 60 },
-        { 768: 80 },
-      ])
+      breakpoints('width', 'vw', {
+        1600: 40,
+        1400: 40,
+        1280: 60,
+        992: 60,
+        768: 80,
+      })
     ).toStrictEqual([
-      '\n' +
-        '    @media screen and (max-width: 1600px) {\n' +
-        '      width: 40vw ;\n' +
-        '    }\n' +
-        '    \n' +
-        '    @media screen and (max-width: 1400px) {\n' +
-        '      width: 40vw ;\n' +
-        '    }\n' +
-        '    \n' +
-        '    @media screen and (max-width: 1400px) {\n' +
-        '      width: 40vw ;\n' +
-        '    }\n' +
-        '    \n' +
-        '    @media screen and (max-width: 1280px) {\n' +
-        '      width: 60vw ;\n' +
-        '    }\n' +
-        '    \n' +
-        '    @media screen and (max-width: 992px) {\n' +
-        '      width: 60vw ;\n' +
-        '    }\n' +
-        '    \n' +
-        '    @media screen and (max-width: 768px) {\n' +
-        '      width: 80vw ;\n' +
-        '    }\n' +
-        '    ',
+      `
+        @media screen and (max-width: 768px) {
+          width: 80vw;
+        }
+        @media screen and (max-width: 992px) {
+          width: 60vw;
+        }
+        @media screen and (max-width: 1280px) {
+          width: 60vw;
+        }
+        @media screen and (max-width: 1400px) {
+          width: 40vw;
+        }
+        @media screen and (max-width: 1600px) {
+          width: 40vw;
+        }`,
     ]);
   });
 });
